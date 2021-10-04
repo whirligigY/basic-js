e9e75f6d945e1116c0c0b959472cfce6e487268f
Yulia <yulia.v@Yulias-MacBook.local>
2021-10-04 18:33:16 +0700

    fix: transform array task fix

--- src/transform-array.js
+++ src/transform-array.js
@@ -16,34 +16,33 @@ import { NotImplementedError } from "../extensions/index.js";
 export default function transform(arr) {
   if (!Array.isArray(arr))
     throw new Error("'arr' parameter must be an instance of the Array!");
-  if (arr && arr.map) {
-    let updatedArr = [...arr].flat(6).reduce(
-      (acc, ar, index) => {
-        if (Array.isArray(ar)) return transform(ar);
-        if (ar === "--double-next") {
-          acc[index + 1] && acc[index + 1] !== null
-            ? acc.splice(index, 1, acc[index + 1])
-            : acc.splice(index, 1);
-        }
-        if (ar === "--double-prev") {
-          acc[index - 1] && acc[index + 1] !== null
-            ? acc.splice(index, 1, acc[index - 1])
-            : acc.splice(index, 1);
-        }
-        if (ar === "--discard-prev") {
-          acc[index - 1] && acc[index + 1] !== null
-            ? acc.splice(index - 1, 2, null, null)
-            : acc.splice(index, 1);
-        }
-        if (ar === "--discard-next") {
-          acc[index + 1] && acc[index + 1] !== null
-            ? acc.splice(index, 2, null, null)
-            : acc.splice(index, 1);
-        }
-        return acc;
-      },
-      [...arr]
-    );
-    return updatedArr.filter((el) => el);
-  }
+
+  let updatedArr = [...arr].flat(5).reduce(
+    (acc, ar, index) => {
+      if (Array.isArray(ar)) return transform(ar);
+      if (ar === "--double-next") {
+        acc[index + 1] && acc[index + 1] !== null
+          ? acc.splice(index, 1, acc[index + 1])
+          : acc.splice(index, 1);
+      }
+      if (ar === "--double-prev") {
+        acc[index - 1] && acc[index + 1] !== null
+          ? acc.splice(index, 1, acc[index - 1])
+          : acc.splice(index, 1);
+      }
+      if (ar === "--discard-prev") {
+        acc[index - 1] && acc[index + 1] !== null
+          ? acc.splice(index - 1, 2, null, null)
+          : acc.splice(index, 1);
+      }
+      if (ar === "--discard-next") {
+        acc[index + 1] && acc[index + 1] !== null
+          ? acc.splice(index, 2, null, null)
+          : acc.splice(index, 1);
+      }
+      return acc;
+    },
+    [...arr]
+  );
+  return updatedArr.filter((el) => el);
 }
